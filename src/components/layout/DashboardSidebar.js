9c783abddb8a4bd8d6367c50a9b03dc517d8801b
Mohammadreza <ma3968312@gmail.com>
2025-05-12 21:05:19 +0330

    add sidebar

---
+++ src/components/layout/DashboardSidebar.js
@@ -0,0 +1,25 @@
+import { getServerSession } from "next-auth";
+import { CgProfile } from "react-icons/cg";
+import styles from "@/layout/DashboardSidebar.module.css";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Link from "next/link";
+
+async function DashboardSidebar({ children }) {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <CgProfile />
+        <p>{session.user.email}</p>
+        <span></span>
+        <Link href={"/dashboard"}>حساب کاربری</Link>
+        <Link href={"/dashboard/my-profile"}>آگهی های من</Link>
+        <Link href={"/dashboard/add"}>ثبت آگهی</Link>
+      </div>
+      <div className={styles.main}>{children}</div>
+    </div>
+  );
+}
+
+export default DashboardSidebar;
