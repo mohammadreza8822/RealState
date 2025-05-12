daf509d66b75ca5f894e7c753ba6bfc6d50351b8
Mohammadreza <ma3968312@gmail.com>
2025-05-12 20:14:42 +0330

    client side authorization

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,15 @@
-import { yekan } from "@/utils/fonts";
 import Layout from "@/layout/Layout";
+import { yekan } from "@/utils/fonts";
 import "./globals.css";
+import NextAuthProvider from "@/provides/NextAuthProvider";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
-        <Layout>{children}</Layout>
+        <NextAuthProvider>
+          <Layout>{children}</Layout>
+        </NextAuthProvider>
       </body>
     </html>
   );
