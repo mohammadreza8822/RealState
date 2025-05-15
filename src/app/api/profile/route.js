4443a81ef31d671e61a72f0aef163255d708a499
Mohammadreza <ma3968312@gmail.com>
2025-05-15 22:42:15 +0330

    create post api for profile

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,85 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import connectDB from "@/utils/connectDB";
+import { Types } from "mongoose";
+import User from "@/models/User";
+import Profile from "@/models/Profile";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    const {
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+    } = body;
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید.",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری شما یافت نشد!" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !title ||
+      !description ||
+      !location ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const newProfile = await Profile.create({
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+      price: +price,
+      userId: new Types.ObjectId(user._id),
+    });
+
+    return NextResponse.json(
+      { message: "آگهی جدید اضافه شد" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است." },
+      { status: 500 }
+    );
+  }
+}
