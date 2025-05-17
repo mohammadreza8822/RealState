a70a168152ed821e5aaf39ab47983e77387045b6
Mohammadreza <ma3968312@gmail.com>
2025-05-17 12:00:12 +0330

    create delete api

---
+++ src/app/api/profile/delete/[profileId]/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import connectDB from "@/utils/connectDB";
+import Profile from "@/models/Profile";
+import User from "@/models/User";
+
+export async function DELETE(req, context) {
+  try {
+    await connectDB();
+
+    const id = context.params.profileId;
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید.",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری شما یافت نشد!" },
+        { status: 404 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        { error: "دسترسی شما به آگهی محدود شده است" },
+        { status: 403 }
+      );
+    }
+
+    await Profile.deleteOne({ _id: id });
+
+    return NextResponse.json(
+      { message: "آگهی مورد نظر حذف شد" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
